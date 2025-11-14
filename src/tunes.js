01684c34807bce3735df306fc7feccd902d64b9f
chasy307 <chasy307@mymail.unisa.edu.au>
2025-11-14 15:30:03 +1030

    fixed instruments selector option

--- src/tunes.js
+++ src/tunes.js
@@ -11,9 +11,9 @@ const gain_patterns = [
 ]
 
 const drum_structure = [
-"~",
-"x*4",
-"{x ~!9 x ~!5 x ~ x ~!7 x ~!3 < ~ x > ~}%16",
+  "~",
+  "x*4",
+  "{x ~!9 x ~!5 x ~ x ~!7 x ~!3 < ~ x > ~}%16",
 ]
 
 const basslines = [
@@ -22,17 +22,17 @@ const basslines = [
 ]
 
 const arpeggiator1 = [
-"{d4 bb3 eb3 d3 bb2 eb2}%16",
-"{c4 bb3 f3 c3 bb2 f2}%16",
-"{d4 bb3 g3 d3 bb2 g2}%16",
-"{c4 bb3 f3 c3 bb2 f2}%16",
+  "{d4 bb3 eb3 d3 bb2 eb2}%16",
+  "{c4 bb3 f3 c3 bb2 f2}%16",
+  "{d4 bb3 g3 d3 bb2 g2}%16",
+  "{c4 bb3 f3 c3 bb2 f2}%16",
 ]
 
 const arpeggiator2 = [
-"{d4 bb3 eb3 d3 bb2 eb2}%16",
-"{c4 bb3 f3 c3 bb2 f2}%16",
-"{d4 bb3 g3 d3 bb2 g2}%16",
-"{d5 bb4 g4 d4 bb3 g3 d4 bb3 eb3 d3 bb2 eb2}%16",
+  "{d4 bb3 eb3 d3 bb2 eb2}%16",
+  "{c4 bb3 f3 c3 bb2 f2}%16",
+  "{d4 bb3 g3 d3 bb2 g2}%16",
+  "{d5 bb4 g4 d4 bb3 g3 d4 bb3 eb3 d3 bb2 eb2}%16",
 ]
 
 const pattern = 0
@@ -40,17 +40,15 @@ const bass = 0
 
 bassline:
 note(pick(basslines, bass))
-.sound("supersaw")
+.sound("<instrument>")
 .postgain(2)
 .room(<reverb>)
 .lpf(700)
-.room(<reverb>)
-.size(<reverb> * 4)
 .postgain(pick(gain_patterns, pattern))
 
 main_arp:
 note(pick(arpeggiator1, "<0 1 2 3>/2"))
-.sound("supersaw")
+.sound("<instrument>")
 .lpf(300)
 .adsr("0:0:.5:.1")
 .room(<reverb>)
@@ -60,44 +58,50 @@ note(pick(arpeggiator1, "<0 1 2 3>/2"))
 drums:
 stack(
   s("tech:5")
-  .postgain(6)
-  .pcurve(2)
-  .pdec(1)
-  .struct(pick(drum_structure, pattern)),
+    .postgain(6)
+    .pcurve(2)
+    .pdec(1)
+    .struct(pick(drum_structure, pattern)),
 
-  s("sh").struct("[x!3 ~!2 x!10 ~]")
-  .postgain(0.5).lpf(7000)
-  .bank("RolandTR808")
-  .speed(0.8)
-  .jux(rev)
-  .room(<reverb>)
-  .gain(0.6),
-  .size(<reverb> * 4)
-
+  s("sh")
+    .struct("[x!3 ~!2 x!10 ~]")
+    .postgain(0.5)
+    .lpf(7000)
+    .bank("RolandTR808")
+    .speed(0.8)
+    .jux(rev)
+    .gain(0.6),
 
   s("{~ ~ rim ~ cp ~ rim cp ~!2 rim ~ cp ~ < rim ~ >!2}%8 *2")
-  .bank("[KorgDDM110, OberheimDmx]").speed(1.2)
-  .postgain(.25)
+    .bank("[KorgDDM110, OberheimDmx]")
+    .speed(1.2)
+    .postgain(.25)
 )
-.room(<reverb>)
 
 drums2:
 stack(
-  s("[~ hh]*4").bank("RolandTR808").room(<reverb>).speed(0.75).gain(1.2),
-  s("hh").struct("x*16").bank("RolandTR808")
-  .gain(0.6)
-  .jux(rev)
-  .room(<reverb>)
-  .postgain(0.5),
+  s("[~ hh]*4")
+    .bank("RolandTR808")
+    .room(<reverb>)
+    .speed(0.75)
+    .gain(1.2),
+
+  s("hh")
+    .struct("x*16")
+    .bank("RolandTR808")
+    .gain(0.6)
+    .jux(rev)
+    .room(<reverb>)
+    .postgain(0.5),
 
   s("[psr:[2|5|6|7|8|9|12|24|25]*16]?0.1")
-  .gain(0.1)
-  .postgain(pick(gain_patterns, pattern))
-  .hpf(1000)
-  .speed(0.5)
-  .rarely(jux(rev)),
+    .gain(0.1)
+    .postgain(pick(gain_patterns, pattern))
+    .hpf(1000)
+    .speed(0.5)
+    .rarely(jux(rev))
 )
-.room(<reverb>)
+
 //Remixed and reproduced from Algorave Dave's code found here: https://www.youtube.com/watch?v=ZCcpWzhekEY
 // all(x => x.gain(mouseX.range(0,1)))
 // all(x => x.log())
